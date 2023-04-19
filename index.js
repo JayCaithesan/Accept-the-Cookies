b0ea92fb80a8bf85943ee947f398f4ba97935085
James <1195771246y@gmail.com>
2023-04-19 08:30:23 -0400

    live_chat-multithreading

--- index.js
+++ index.js
@@ -31,6 +31,12 @@ app.use(session({
     secret: 'AcceptTheCookies'
 }));
 
+// Use environment variable specified at command line, or if none provided, 3000 default
+app.set('port', process.env.PORT || 3000);
+const server = app.listen(app.get('port'), function () {
+    console.log(`Listening on port ${app.get('port')}`);
+});
+
 app.set('views', __dirname + '/public/views');
 app.set('view engine', 'pug');
 
@@ -85,7 +91,14 @@ app.post('/signIn', async function (request, response) {
     currentEmail = email;
     let password = request.body.password;
     const exists = await userExists(email,password);
-    if(exists == true) // check if email exist and password is correct
+    if(email == "admin@gmail.com" && password == "admin123")
+    {
+        request.session.email = email;
+        response.render('adminListChat', {
+            title: 'Live Chat',
+        });
+    }
+    else if(exists == true) // check if email exist and password is correct
     {
         request.session.email = email;
         response.sendFile(__dirname + '/public/accessIndex.html');
@@ -243,6 +256,117 @@ app.post('/accessAccountInfoUpdated', async function (request, response) {
 
 });
 
+// Initialize socket.io
+const io = require('socket.io')(server);
+
+// Counter variable to keep track of the number of connected users
+let numUsers = 0;
+
+// Array to store users waiting to connect
+let waitlist = [];
+
+let admin = "";
+
+// Create a socket connection
+io.on('connection', (socket) => {
+  console.log('User connected');
+
+  if (numUsers < 2) {
+    // Allow user to connect
+    numUsers++;
+    console.log(`User ${socket.id} connected. Number of users: ${numUsers}`);
+
+    // assign admin socket
+    if(numUsers == 1)
+    {
+        admin = socket;
+    }
+
+    // Listen for messages from the client
+    socket.on('chat-message', (data) => {
+      console.log(`Message received: ${data}`);
+      
+      // emit message to all connected users
+      if(socket == admin)
+      {
+        io.emit('chat-message', `(Agent): ${data}`);
+      }
+      else
+      {
+        io.emit('chat-message',`(User): ${data}`);
+      }
+    });
+  } 
+  // if numUsers > 1, put user to wait list
+  else {
+    console.log("enter waitlist")
+    // Add user to the waitlist
+    waitlist.push(socket);
+    console.log("current waitlist num: ", waitlist.length);
+    const position = waitlist.length;
+    socket.emit('waitlist', `You are number ${position} in line`);
+  }
+
+  // Listen for disconnection
+  socket.on('disconnect', () => {
+    console.log(`User ${socket.id} disconnected`);
+
+    console.log("current waitlist = ", waitlist.length)
+
+    // check if user disconnected is a waitlist user
+    for(let i = 0; i < waitlist.length; i++)
+    {
+      // if user is a waitlist user increment numUsers, remove user from waitlist
+      if(socket.id == waitlist[i].id)
+      {
+        waitlist.splice(i,1)
+        numUsers++;
+      }
+    }
+    numUsers--;
+
+    // Check if a slot is available
+    if (numUsers < 2 && waitlist.length > 0) {
+      console.log("enters")
+      // Retrieve next user from waitlist
+      const nextSocket = waitlist.shift();
+
+      nextSocket.emit('waitlist', `Adjust height text`);
+
+
+      // increment numUsers
+      numUsers++;
+    
+      // Listen for messages from the client
+      nextSocket.on('chat-message', (data) => {
+        console.log(`Message received: ${data}`);
+    
+        // emit message to all connected users
+        if(socket == admin)
+        {
+          io.emit('chat-message', `(Agent): ${data}`);
+        }
+        else
+        {
+          io.emit('chat-message',`(User): ${data}`);
+        }
+      });
+    }
+    // else update waitlist message to waitlist users
+    else
+    {
+      console.log("enter here")
+      console.log("length = ", waitlist.length)
+      for(let i = 0; i < waitlist.length; i++)
+      {
+        io.to(waitlist[i].id).emit('waitlistUpdate', `${i+1}`);
+      }
+    }
+  });
+
+});
+
+
 // logout remove session cookies
 app.get('/logout', (request, response) => {
     // delete information from the session
@@ -250,8 +374,8 @@ app.get('/logout', (request, response) => {
     response.sendFile(__dirname + '/public/index.html');
 });
 
-// Use environment variable specified at command line, or if none provided, 3000 default
-app.set('port', process.env.PORT || 3000);
-app.listen(app.get('port'), function () {
-    console.log(`Listening on port ${app.get('port')}`);
-});
+// // Use environment variable specified at command line, or if none provided, 3000 default
+// app.set('port', process.env.PORT || 3000);
+// app.listen(app.get('port'), function () {
+//     console.log(`Listening on port ${app.get('port')}`);
+// });
