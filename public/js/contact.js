1940bbe16fb41dba5e399dfc7230eb147defa640
ShianLiChen <shianli.chen@ontariotechu.net>
2023-04-19 13:05:52 -0400

    update contact page logic and add some comments

--- public/js/contact.js
+++ public/js/contact.js
@@ -1,7 +1,9 @@
 $(document).ready(function(){
     
 })
+// Function that saves the inputted data from the customer 
 function saveRecord(){
+    // Regex for validation of an email 
     let validEmail = /^\w*(@)[a-zA-Z]*((\.)[a-zA-Z]{2,3})+$/;
     let validPhone = /^\([0-9]{3}\)\-[0-9]{3}\-[0-9]{4}$/;
     let email = $("#emailInput").val();
@@ -10,18 +12,36 @@ function saveRecord(){
     let message = $("#messageInput").val();
     let purpose = $("#purposeSelect").val();
 
-    if(!email.match(validEmail)){
+    if(!email.match(validEmail) && !phone.match(validPhone)){
         console.log("Not a valid email");
-        $("#emailTitle").append("<div class=\"warning\" style=\"display:inline\"> Make sure your input is in the correct format</div>");
-        $("#emailTitle").css("background-color", "red");
-    }
-    
-        if(!phone.match(validPhone)){
-            console.log("Not a valid phone number");
+        if(!$("#emailTitle").html().includes("warning"))
+        {
+            $("#emailTitle").append("<div class=\"warning\" style=\"display:inline\"> Make sure your input is in the correct format</div>");
+            $("#emailTitle").css("background-color", "red");
+        }
+        console.log("Not a valid phone number");
+        if(!$("#phoneNumberTitle").html().includes("warning")){
             $("#phoneNumberTitle").append("<div class=\"warning\" style=\"display:inline\"> Make sure your input is in the correct format</div>");
             $("#phoneNumberTitle").css("background-color", "red");
         }
-    else{
+    }
+    else if(!email.match(validEmail) && phone.match(validPhone)){
+        $("#phoneNumberTitle").css("background-color", "#FFE6D6");
+        $("#phoneNumberTitle .warning").remove();
+        if(!$("#emailTitle").html().includes("warning")){
+            $("#emailTitle").append("<div class=\"warning\" style=\"display:inline\"> Make sure your input is in the correct format</div>");
+            $("#emailTitle").css("background-color", "red");
+        }
+    }
+    else if(email.match(validEmail) && !phone.match(validPhone)){
+        $("#emailTitle").css("background-color", "#FFE6D6");
+        $("#emailTitle .warning").remove();
+        if(!$("#phoneNumberTitle").html().includes("warning")){
+            $("#phoneNumberTitle").append("<div class=\"warning\" style=\"display:inline\"> Make sure your input is in the correct format</div>");
+            $("#phoneNumberTitle").css("background-color", "red");
+        }
+    }
+    else if(phone.match(validPhone) && email.match(validEmail)){
         console.log("Valid phone number");
         $("#emailTitle").css("background-color", "#FFE6D6");
         $("#phoneNumberTitle").css("background-color", "#FFE6D6");
